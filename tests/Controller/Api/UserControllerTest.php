660b735e9493792aa45d27ec1e7214562215be82
Ayoub Moutii <youbirox1994@gmail.com>
2026-01-28 21:53:43 +0100

    Add User Test

--- tests/Controller/Api/UserControllerTest.php
+++ tests/Controller/Api/UserControllerTest.php
@@ -14,10 +14,21 @@ final class UserControllerTest extends WebTestCase
         $container = static::getContainer();
         $client->request('GET', '/api/users');
 
-        $user = $container->get('doctrine')
-            ->getRepository(User::class)
-            ->findOneBy(['email' => 'admin@monsite.com']);
+        // $user = $container->get('doctrine')
+        //     ->getRepository(User::class)
+        //     ->findOneBy(['email' => 'admin@monsite.com']);
 
+        $em = $container->get('doctrine')->getManager();
+        $user = new User();
+        $user->setEmail('test@test.com');
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword(
+            $container->get(UserPasswordHasherInterface::class)
+                ->hashPassword($user, 'password')
+        );
+
+        $em->persist($user);
+        $em->flush();
     
         $jwtManager = $container->get(JWTTokenManagerInterface::class);
         $token = $jwtManager->create($user);
